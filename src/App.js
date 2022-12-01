20b7f43823a2e4a7ad602a02c1c5e3170a5057c6
YanChen <yc.yanchen@hotmail.com>
2022-11-30 19:59:43 -0500

    Added a header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import HeaderMain from "./HeaderMain";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App wrapper">
+			<HeaderMain />
+		</div>
+	);
 }
 
 export default App;
+
+// On page load, mount the header, + button, and list of previous bills from firebase
+// parse the data from firebase then mount the components to the page within a useEffect with an empty dependency array
+// use a prop to send the data to the component which will display all previous bills
+
+// Mount the New Bill related components to the page when user presses the + button on the main page
+// this includes a form which has the inputs for the bill total and the number of ways it would be split
+
+// In the input forms, use onChange to collect the information user is inputting
+// store the information into a useState when user presses the submit button before processing it to firebase
+
+// Submitting will also mount the main page components which will automatically retrieve previous bills
---
+++ src/HeaderMain.js
@@ -0,0 +1,12 @@
+const HeaderMain = () => {
+	return (
+		<header className="header-main">
+			<h1>Split My Bill</h1>
+			<div className="add-bill">
+				<p className="plus-symbol">+</p>
+			</div>
+		</header>
+	);
+};
+
+export default HeaderMain;
