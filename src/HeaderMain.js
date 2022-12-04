46d021e5a6a8e4822e3675251bcaa1cff07deb10
YanChen <yc.yanchen@hotmail.com>
2022-12-04 15:43:26 -0500

    Made some styling changes for touch accessibility

--- src/HeaderMain.js
+++ src/HeaderMain.js
@@ -3,7 +3,7 @@ const HeaderMain = (props) => {
 		<header className="header-main">
 			<h1>Split My Bill</h1>
 			<button onClick={props.changeDisplay}>
-				<span class="material-symbols-outlined button-style button-round button-add ">add</span>
+				<span class="material-symbols-outlined button-style button-round button-bottom-float ">add</span>
 			</button>
 		</header>
 	);
